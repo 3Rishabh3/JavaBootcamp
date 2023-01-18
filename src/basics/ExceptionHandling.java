494189548a850d5559b4e7e65127329d0dd59e56
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-18 17:23:58 +0530

    Exception handling

--- src/basics/ExceptionHandling.java
+++ src/basics/ExceptionHandling.java
@@ -9,7 +9,7 @@ class DefaultException extends Exception {
 public class ExceptionHandling {
     public static void main(String[] args){
         int[] nums = new int[5];
-        int i = 3;
+        int i = 3; // Try this with i > 4
         try{
             if(nums[i] == 0)
                 throw new DefaultException();
