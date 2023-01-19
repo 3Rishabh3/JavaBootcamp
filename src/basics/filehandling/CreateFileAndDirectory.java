f4fb7d88a00b9a01ccd4ce5f6966cd34a5493002
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-19 14:59:55 +0530

    file handling

--- src/basics/filehandling/CreateFileAndDirectory.java
+++ src/basics/filehandling/CreateFileAndDirectory.java
@@ -4,12 +4,10 @@ import java.io.*;
 
 public class CreateFileAndDirectory {
     public static void main(String[] args) throws IOException {
-        File f = new File("/FileHandling/1_CreateFile");
-        System.out.println(f.exists());
-        f.createNewFile();
-        System.out.println(f.exists());
-
         File d = new File("src/basics/filehandling/files");
         d.mkdir();
+
+        File f = new File("src/basics/filehandling/files/1_CreateFile.txt");
+        f.createNewFile();
     }
 }
