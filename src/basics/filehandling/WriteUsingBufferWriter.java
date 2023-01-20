7afc615877d852ca2ec4a00896aacca0a8ef85d9
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-20 11:15:36 +0530

    Write using buffer reader

--- src/basics/filehandling/WriteUsingBufferWriter.java
+++ src/basics/filehandling/WriteUsingBufferWriter.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class WriteUsingBufferWriter {
     public static void main(String[] args) throws IOException {
         FileWriter fw = new FileWriter("src/basics/filehandling/files/2_ExperimentFile.txt", true);
-        BufferedWriter bf = new BufferedWriter(fw);
-        bf.write("Writing using buffer reader");
-        bf.close();
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("Writing using buffer reader");
+        bw.close();
     }
 }
