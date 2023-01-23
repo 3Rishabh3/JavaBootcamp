85098fd0539c0693aeca51c1f7b50e2ac9190659
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-23 18:04:01 +0530

    Enums

--- src/basics/Enums.java
+++ src/basics/Enums.java
@@ -6,6 +6,7 @@ enum Status {
 
 public class Enums {
     public static void main(String[] args) {
+
         Status s = Status.Failed;
         System.out.println(s);
         Status[] st = Status.values();
@@ -13,5 +14,12 @@ public class Enums {
         for(Status ss : st){
             System.out.println(ss + " " +ss.ordinal());
         }
+
+        // TODO Convert string to enum
+        String str = "Running";
+        // Cover corner cases also: if u get empty string then handle it
+        // TODO Convert enum value to string
     }
 }
+
+
