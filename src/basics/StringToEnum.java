4a875ffc22ac706112b8ba9e5e38eaab74547827
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-23 17:30:14 +0530

    String to enums in java

---
+++ src/basics/StringToEnum.java
@@ -0,0 +1,12 @@
+package basics;
+
+enum Color {
+    RED, GREEN, BLUE;
+}
+public class StringToEnum {
+        public static void main(String[] args) {
+        String input = "GREEN";
+        Color color = Color.valueOf(input);
+        System.out.println(color);
+    }
+}
