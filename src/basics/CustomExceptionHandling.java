4c1100a16120793c6033da37c9b9fa696cec210a
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-25 11:05:25 +0530

    Custom exception handling

---
+++ src/basics/CustomExceptionHandling.java
@@ -0,0 +1,35 @@
+package basics;
+//CustomExceptionHandling
+
+import java.io.*;
+import java.sql.*;
+
+class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
+
+public class CustomExceptionHandling {
+    public static void main(String[] args) throws CustomException {
+        try {
+            // Example of handling IOException
+            FileInputStream file = new FileInputStream("src/basics/filehandling/files/2_ExperimentFile.txt");
+
+            // Example of handling ArithmeticException
+            int a = 10, b = 1;
+            int c = a / b;
+
+            // Example of handling SQL Exception
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "username", "password");
+
+            // Example of handling IndexOutOfBoundsException
+            int[] arr = new int[5];
+            int index = 10;
+            int value = arr[index];
+
+        } catch (IOException | ArithmeticException | SQLException | IndexOutOfBoundsException e) {
+            throw new CustomException("An exception occurred: " + e.getMessage());
+        }
+    }
+}
