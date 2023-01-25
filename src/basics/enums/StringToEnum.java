18efc7952f3d0afa9e132452686d539d8af11b11
Rishabh Gupta <3rishabh3@gmail.com>
2023-01-25 16:39:04 +0530

    String to enums

--- src/basics/enums/StringToEnum.java
+++ src/basics/enums/StringToEnum.java
@@ -1,12 +1,26 @@
 package basics.enums;
 
+import java.util.Scanner;
+
 enum Color {
     RED, GREEN, BLUE;
 }
 public class StringToEnum {
         public static void main(String[] args) {
-        String input = "GREEN";
-        Color color = Color.valueOf(input);
-        System.out.println(color);
+            Scanner scanner = new Scanner(System.in);
+            boolean match = false;
+            while (!match) {
+                System.out.println("Enter Colour Name: ");
+                String colorName = scanner.nextLine();
+                try {
+                    Color.valueOf(colorName.toUpperCase());
+                    System.out.println("Match success: " + colorName);
+                    match = true;
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Color name not matched.");
+                    System.out.println("Plz enter correct Color name");
+                }
+            }
     }
 }
+
